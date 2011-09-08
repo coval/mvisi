a9b014a2a67e8d990fe2f28d5b105bc54d617472
Pawel Kowalczyk <covall@gmail.com>
2011-09-08 17:25:19 +0200

    style improvements

--- settings.py
+++ settings.py
@@ -31,7 +31,7 @@ TIME_ZONE = 'Europe/Warsaw'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = 'en'
 
 SITE_ID = 1
 
